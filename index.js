5fb8567d7c2eea9ed4dcd27dd7f8778cd65ab9f4
Itadori999 <pranav.geeko@outlook.com>
2022-07-04 19:55:17 +0530

    theme switch

--- index.js
+++ index.js
@@ -2,26 +2,32 @@
 let form = document.querySelector("form");
 let text = document.getElementById("text");
 let todoCon = document.querySelector(".todo-con");
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    addtodo();
-})
- let todos=JSON.parse(localStorage.getItem("todos"));
- if(todos){
- todos.forEach(element=>{
-addtodo(element)
- });
- }
-function addtodo(){
-    let todoColl = document.createElement("div");
-    let todotext=text.value;
-    todoColl.innerHTML=`
+const themeswitch = document.getElementById("theme-toggle");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addtodo();
+});
+let todos = JSON.parse(localStorage.getItem("todos"));
+if (todos) {
+  todos.forEach((element) => {
+    console.log(element);
+    addtodo(element);
+  });
+}
+function addtodo() {
+  let todoColl = document.createElement("div");
+  let todotext = text.value;
+  todoColl.innerHTML = `
     <div class="todo-li">
             <div class="check"><img src="./Images/icon-check.svg" alt=""></div>
             <p class="ptag">${todotext}</p>
             <button class="close"><img src="./Images/icon-cross.svg" alt=""></button>
             </div>
             <div class="hr"></div>
-            `
-        todoCon.appendChild(todoColl)
-} 
+            `;
+  todoCon.appendChild(todoColl);
+}
+themeswitch.addEventListener("click", themeswitcher);
+function themeswitcher(e){
+    themeLogos.forEach(logo => logo.classlist.toggle("todo"))
+}
